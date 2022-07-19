b0c48185eec3c6740e2c2dee2dad15372db3d49d
Dhwl <dhawalpawanarkar123@gmail.com>
2022-07-19 21:26:45 +0530

    changed the min fee value to 4000 to see changes in MongoDB

--- student.js
+++ student.js
@@ -1,14 +1,10 @@
 import mongoose from 'mongoose';
-
-//defining Schema
 const studentSchema = new mongoose.Schema({
     name:{type:String,required:true,trim:true},
     age:{type:Number,required:true},
     fees:{type:mongoose.Decimal128,required:true,validate:(value)=>{
-        value>=5000.0
+        value>=4000.0
     }}
 })
-
-//model
 const StudentModel = mongoose.model("student",studentSchema);
 export default StudentModel;
